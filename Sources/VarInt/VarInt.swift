aaa68a066a441a293f1bf612bae65aca75946c95
Brandon <32753167+btoms20@users.noreply.github.com>
2025-02-27 18:40:04 -0800

    Added license header

--- Sources/VarInt/VarInt.swift
+++ Sources/VarInt/VarInt.swift
@@ -1,6 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2025 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+
 //
 //  VarInt.swift
-//  VarInt
 //
 //  Created by Teo on 01/10/15.
 //  Licensed under MIT See LICENCE file in the root of this project for details.
